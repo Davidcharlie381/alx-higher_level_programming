8faeae49ceecf4787a2ad8ed1be262a9a838cd6f
Davidcharlie381 <davidcharleswisdom@gmail.com>
2022-05-31 23:23:00 +0100

    refactor: code structure

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+
+F = "Fizz"
+B = "Buzz"
+
 def fizzbuzz():
     num = 1
     while num <= 100:
-        if num % 3 == 0:
-            print("Fizz ", end='')
-        elif num % 5 == 0:
-            print("Buzz ", end='')
-        elif num % 3 and num % 5 == 0:
-            print("FizzBuzz ", end='')
+        if (num % 3 and num % 5 == 0):
+            print("{}{}".format(F, B), end='')
+        elif num % 5:
+            print("{}".format(B), end='')
+        elif num % 3:
+            print("{}".format(F), end='')
         else:
             print(num, end='')
         num += 1
