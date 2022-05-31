b705caf6a99f3651442e2429be2fddc4a8e95f05
Davidcharlie381 <davidcharleswisdom@gmail.com>
2022-05-31 23:37:03 +0100

    add: code brackets

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,9 +7,9 @@ def fizzbuzz():
     for num in range(1, 101):
         if (num % 3 and num % 5):
             print("%s%s" % (F, B), end=' ')
-        elif num % 3:
+        elif (num % 3):
             print("%s" % (F), end=' ')
-        elif num % 5:
+        elif (num % 5):
             print("%s" % (B), end=' ')
         else:
             print("%d" % (num), end=' ')
