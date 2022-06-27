a01425e28510776d8a549dfd0a45cbd382ce9ed4
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2022-06-27 23:30:57 +0100

    Create 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This is the '0-rectangle' module
+Created on Jun 27, 2022
+"""
+
+
+class Rectangle():
+    """Empty Class Rectangle that defines a has no method"""
+    pass
